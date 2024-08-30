b5ac2e463bfcb93ca6df19124f63f5ed2dfafd17
Chen <chpyer@gmail.com>
2024-08-30 17:24:25 +0800

    0.17

---
+++ migration/2024-02-24.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "sweep_schedules" ADD COLUMN "enabled_status" bigint NULL DEFAULT 1;
---
+++ migration/2024-03-10.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "admins" ADD COLUMN "wechat_openid" character varying NULL;
+ALTER TABLE "risks" ADD COLUMN "maintained_images" jsonb NULL;
